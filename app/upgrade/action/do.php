4a054c01efb27cfb6c1ff8fd5eb11135b2faef25
thinksaascn <thinksaas@qq.com>
2011-12-10 18:37:34 +0800

    更新1203到1210的更新程序

--- app/upgrade/action/do.php
+++ app/upgrade/action/do.php
@@ -1,63 +1,16 @@
 <?php
 defined('IN_TS') or die('Access Denied.');
-//1.5-1126到1.5-1203升级
+//1.5-1203到1.5-1210升级
 
 
-if($db->once_num_rows("DESCRIBE ".dbprefix."tag count_article")== '0'){
-	$db->query("ALTER TABLE `".dbprefix."tag` ADD `count_article` INT( 11 ) NOT NULL DEFAULT '0' COMMENT 
-'统计文章' AFTER `count_topic`;");
+if($db->once_num_rows("DESCRIBE ".dbprefix."photo_album isrecommend")== '0'){
+	$db->query("ALTER TABLE `".dbprefix."photo_album` ADD `isrecommend` TINYINT( 1 ) NOT NULL DEFAULT '0' COMMENT '是否推荐' AFTER `count_view`;");
+	$db->query("ALTER TABLE `ts_photo_album` ADD INDEX ( `isrecommend` );");
 }
 
-//添加几个表
-$db->query("DROP TABLE IF EXISTS `".dbprefix."article`, `".dbprefix."article_cate`,`".dbprefix."article_comment`,`".dbprefix."feed`,`".dbprefix."tag_article_index`");
+$db->query("ALTER TABLE `".dbprefix."feed` CHANGE `template` `template` VARCHAR( 1024 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '动态模板'");
 
-//添加ts_article
-$db->query("CREATE TABLE IF NOT EXISTS `".dbprefix."article` (
-  `articleid` int(11) NOT NULL AUTO_INCREMENT COMMENT '文章ID',
-  `userid` int(11) NOT NULL DEFAULT '0' COMMENT '用户ID',
-  `cateid` int(11) NOT NULL DEFAULT '0' COMMENT '分类ID',
-  `title` char(64) NOT NULL DEFAULT '' COMMENT '标题',
-  `content` text NOT NULL COMMENT '内容',
-  `isphoto` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否有图片',
-  `isattach` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否有附件',
-  `isaudit` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否审核',
-  `addtime` int(11) NOT NULL DEFAULT '0' COMMENT '时间',
-  PRIMARY KEY (`articleid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;");
-
-$db->query("CREATE TABLE IF NOT EXISTS `".dbprefix."article_cate` (
-  `cateid` int(11) NOT NULL AUTO_INCREMENT COMMENT '分类ID',
-  `catename` char(16) NOT NULL DEFAULT '' COMMENT '分类名称',
-  `orderid` int(11) NOT NULL DEFAULT '0' COMMENT '排序ID',
-  PRIMARY KEY (`cateid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;");
-
-$db->query("CREATE TABLE IF NOT EXISTS `".dbprefix."article_comment` (
-  `commentid` int(11) NOT NULL AUTO_INCREMENT,
-  `articleid` int(11) NOT NULL DEFAULT '0' COMMENT '文章ID',
-  `userid` int(11) NOT NULL DEFAULT '0' COMMENT '用户ID',
-  `content` text NOT NULL COMMENT '评论内容',
-  `addtime` int(11) NOT NULL DEFAULT '0' COMMENT '时间',
-  PRIMARY KEY (`commentid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='文章评论' AUTO_INCREMENT=1 ;");
-
-$db->query("CREATE TABLE IF NOT EXISTS `".dbprefix."feed` (
-  `feedid` int(11) NOT NULL AUTO_INCREMENT,
-  `userid` int(11) NOT NULL DEFAULT '0' COMMENT '用户ID',
-  `template` varchar(300) NOT NULL DEFAULT '' COMMENT '动态模板',
-  `data` varchar(600) NOT NULL DEFAULT '' COMMENT '动态数据',
-  `addtime` int(11) NOT NULL DEFAULT '0' COMMENT '时间',
-  PRIMARY KEY (`feedid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='全站动态' AUTO_INCREMENT=1 ;");
-
-$db->query("CREATE TABLE IF NOT EXISTS `".dbprefix."tag_article_index` (
-  `articleid` int(11) NOT NULL DEFAULT '0' COMMENT '帖子ID',
-  `tagid` int(11) NOT NULL DEFAULT '0',
-  UNIQUE KEY `articleid_2` (`articleid`,`tagid`),
-  KEY `articleid` (`articleid`),
-  KEY `tagid` (`tagid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
-
+$db->query("ALTER TABLE `".dbprefix."feed` CHANGE `data` `data` VARCHAR( 1024 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '动态数据'");
 
 //////////////////////////////////此处向下不用修改////////////////////////////////////
 
