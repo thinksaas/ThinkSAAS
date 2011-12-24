cd2bcd93d21979733516760fa9b3cb00c2229d62
thinksaascn <thinksaas@qq.com>
2011-12-24 17:12:08 +0800

    完善苹果机后台

--- app/apple/about.php
+++ app/apple/about.php
@@ -8,7 +8,7 @@ return array (
   'email' => 'thinksaas@qq.com',
   'author' => '邱君',
   'author_url' => 'http://www.thinksaas.cn',
-  'isoption' => '0',
+  'isoption' => '1',
   'isinstall' => '1',
   'issql' => '1',
   'issystem' => '1',
