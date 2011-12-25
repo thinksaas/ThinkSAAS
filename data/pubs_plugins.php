590f9b693399e500f183806b08ff740d607dfd7a
thinksaascn <thinksaas@qq.com>
2011-12-25 18:45:44 +0800

    data/pubs_plugins.php
    Signed-off-by: thinksaascn <thinksaas@qq.com>

--- data/pubs_plugins.php
+++ data/pubs_plugins.php
@@ -1,10 +1,8 @@
 <?php
 defined('IN_TS') or die('Access Denied.');
 return array (
-  0 => 'qq',
   1 => 'navs',
   2 => 'gotop',
-  3 => 'gobad',
   4 => 'feedback',
   5 => 'counter',
   6 => 'wast_word',
