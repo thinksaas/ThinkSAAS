87ec2f478fc8d9d642603117e7a6a8ce03f2d86e
thinksaascn <thinksaas@qq.com>
2012-01-14 19:38:40 +0800

    more

--- thinksaas/fun.base.php
+++ thinksaas/fun.base.php
@@ -225,17 +225,17 @@ function getTime($btime, $etime){
 	$mins = intval($rtime / 60);
 	$secs = $rtime % 60;
 	if($days>=1){
-		return $days.' 天前';
+		return $days.L::fun_daysago;
 	}
 	if($hours>=1){
-		return $hours.' 小时前';
+		return $hours.L::fun_hoursago;
 	}
 
 	if($mins>=1){
-		return $mins.' 分钟前';
+		return $mins.L::fun_minutesago;
 	}
 	if($secs>=1){
-		return $secs.' 秒前';
+		return $secs.L::fun_secondsago;
 	}
  
 }
