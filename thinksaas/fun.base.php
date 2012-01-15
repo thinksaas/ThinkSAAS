caa2a6745d1ce710440a54e6035d54089a3ed812
thinksaascn <thinksaas@qq.com>
2012-01-15 14:00:18 +0800

    more

--- thinksaas/fun.base.php
+++ thinksaas/fun.base.php
@@ -180,30 +180,8 @@ function pagination($count,$perlogs,$page,$url,$suffix=''){
 }
 
 //验证Email
-
 function valid_email($email){
-	if (!@ereg("^[^@]{1,64}@[^@]{1,255}$", $email)){ 
-		return false; 
-	}
-	$email_array		= explode("@", $email); 
-	$local_array		= explode(".", $email_array[0]); 
-	for ($i = 0; $i < sizeof($local_array); $i++){ 
-		if (!@ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$", $local_array[$i])){ 
-			return false; 
-		 } 
-	}   
-	if (!@ereg("^\[?[0-9\.]+\]?$", $email_array[1])){
-		$domain_array = explode(".", $email_array[1]); 
-		if (sizeof($domain_array) < 2){ 
-			return false;
-		} 
-		for ($i = 0; $i < sizeof($domain_array); $i++){ 
-			if (!@ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i])){ 
-				return false; 
-			} 
-		} 
-	} 
-	return true;
+	return preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $email);
 } 
 
 //处理时间的函数
