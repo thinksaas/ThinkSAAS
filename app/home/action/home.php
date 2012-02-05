47075da66af232fd41e37bfaa94dcf24e4855bfc
thinksaas <qiniao@vip.qq.com>
2012-02-05 20:59:10 +0800

    陆续删除语言包

--- app/home/action/home.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('IN_TS') or die('Access Denied.');
-$userid = intval($TS_USER['user']['userid']);
-
-$arrUserId = $db->fetch_all_assoc("select userid_follow from ".dbprefix."user_follow where `userid`='$userid'");
-
-foreach($arrUserId as $item){
-	
-}
