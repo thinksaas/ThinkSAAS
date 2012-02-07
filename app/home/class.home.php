dba62c8cd6738e3e0da4d3e5dc267fa8858feca8
thinksaascn <thinksaas@qq.com>
2012-02-07 18:31:25 +0800

    测试tsModel

--- app/home/class.home.php
+++ app/home/class.home.php
@@ -1,11 +1,10 @@
 <?php
 defined('IN_TS') or die('Access Denied.');  
+
+//class home extends tsModel{
 class home{
 
-	var $db;
-
-	function home($dbhandle){
-		$this->db = $dbhandle;
-	}
+	//var $pk = "id"; // 每个留言唯一的标志，可以称为主键
+	//var $table = "home"; // 数据表的名称
 
 }
