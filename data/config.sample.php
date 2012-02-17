ec9a0a7ec75390370052c149907d194388e7968c
thinksaascn <thinksaas@qq.com>
2012-02-17 15:11:25 +0800

    config.sample.php
    Signed-off-by: thinksaascn <thinksaas@qq.com>

--- data/config.sample.php
+++ data/config.sample.php
@@ -4,10 +4,11 @@
 	 *数据库配置
 	 *$Id: config.sample.php 465 2011-11-14 07:51:18Z qiujun $
 	 */
-	$TS_DB['sql']='1';
+	$TS_DB['sql']='mysql';
 	$TS_DB['host']='localhost';
+	$TS_DB['port']='3306';
 	$TS_DB['user']='root';
-	$TS_DB['pwd']='123456';
-	$TS_DB['name']='thinksaas_svn';
+	$TS_DB['pwd']='';
+	$TS_DB['name']='thinksaas';
 	$TS_DB['pre']='ts_';
 	define('dbprefix','ts_');
