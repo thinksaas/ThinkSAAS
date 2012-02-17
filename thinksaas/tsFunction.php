8d3447b2a6481c3ace5c53d669e0a719afe62b1c
thinksaascn <thinksaas@qq.com>
2012-02-17 14:25:35 +0800

    tsFunction.php
    Signed-off-by: thinksaascn <thinksaas@qq.com>

--- thinksaas/tsFunction.php
+++ thinksaas/tsFunction.php
@@ -201,28 +201,11 @@ function pagination($count,$perlogs,$page,$url,$suffix=''){
 //验证Email
 
 function valid_email($email){
-	if (!@ereg("^[^@]{1,64}@[^@]{1,255}$", $email)){ 
-		return false; 
+	if(filter_data($email,FILTER_VALIDATE_EMAIL)){
+		return true;
+	}else{
+		return false;
 	}
-	$email_array		= explode("@", $email); 
-	$local_array		= explode(".", $email_array[0]); 
-	for ($i = 0; $i < sizeof($local_array); $i++){ 
-		if (!@ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$", $local_array[$i])){ 
-			return false; 
-		 } 
-	}   
-	if (!@ereg("^\[?[0-9\.]+\]?$", $email_array[1])){
-		$domain_array = explode(".", $email_array[1]); 
-		if (sizeof($domain_array) < 2){ 
-			return false;
-		} 
-		for ($i = 0; $i < sizeof($domain_array); $i++){ 
-			if (!@ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i])){ 
-				return false; 
-			} 
-		} 
-	} 
-	return true;
 } 
 
 //处理时间的函数
