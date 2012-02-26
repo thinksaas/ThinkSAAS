ee537766058194ddf3ffb5f86a99d25d56c9f466
thinksaas <qiniao@vip.qq.com>
2012-02-26 13:13:30 +0800

    home_plugins.php

--- data/home_plugins.php
+++ data/home_plugins.php
@@ -1,5 +1,4 @@
 <?php
 defined('IN_TS') or die('Access Denied.');
 return array (
-  0 => 'links',
 );
--- data/pubs_plugins.php
+++ data/pubs_plugins.php
@@ -1,10 +1,4 @@
 <?php
 defined('IN_TS') or die('Access Denied.');
 return array (
-  1 => 'navs',
-  2 => 'gotop',
-  5 => 'counter',
-  7 => 'feedback',
-  8 => 'wast_word',
-  9 => 'qq2',
 );
