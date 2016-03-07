c8f13d504ab330fd3e558fba2de8aac4b397452e
ThinkSAAS <thinksaas@qq.com>
2016-03-07 09:27:40 +0800

    小优化和修复

--- plugins/pubs/wordad/wordad.php
+++ plugins/pubs/wordad/wordad.php
@@ -3,20 +3,15 @@ defined('IN_TS') or die('Access Denied.');
 //统计代码
 function wordad(){
 	global $tsMySqlCache;
-echo '<style>
-	.wordad{ overflow:hidden;background: none repeat scroll 0 0 #EAEAEA;border: 1px solid #DDDDDD;margin-bottom: 10px;overflow: hidden;padding:5px;}
-	.wordad ul{}
-	.wordad ul li{width:225px;float:left;margin:2px 5px;}
-	</style>';
 	$arrData = fileRead('data/plugins_pubs_wordad.php');
 	if($arrData==''){
 		$arrData = $tsMySqlCache->get('plugins_pubs_wordad');
 	}
 	
-	echo '<div class="wordad"><ul>';
+	echo '<div class="panel panel-default"><div class="panel-body"><div class="row">';
 	foreach($arrData as $key=>$item){
-		echo '<li><a rel="nofollow" target="_blank" href="'.$item['url'].'">'.$item['title'].'</a></li>';
+		echo '<div class="col-md-3"><a target="_blank" href="'.$item['url'].'">'.$item['title'].'</a></div>';
 	}
-	echo '</ul><div class="clear"></div></div>';
+	echo '</div></div></div>';
 }
 addAction('wordad','wordad');
