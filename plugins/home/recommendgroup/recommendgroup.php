bccb20fe5cca67cdcbeaba0ba9e6bf8a75fdc570
ThinkSAAS <thinksaas@qq.com>
2016-11-04 10:04:02 +0800

    细节优化

--- plugins/home/recommendgroup/recommendgroup.php
+++ plugins/home/recommendgroup/recommendgroup.php
@@ -2,38 +2,11 @@
 defined('IN_TS') or die('Access Denied.'); 
 //推荐小组
 function recommendgroup(){
-	
 	$arrRecommendGroup = aac('group')->getRecommendGroup('12');
-	
-	echo '<div class="panel panel-default">';
-	echo '<div class="panel-heading">推荐小组</div>';
-	echo '<div class="panel-body">';
-	foreach($arrRecommendGroup as $key=>$item){
-	$count_user = $item['count_user'];
-	echo '<div class="sub-item">
-	<div class="pic">
-	<a href="'.tsUrl('group','show',array('id'=>$item[groupid])).'">
-	<img class="img-rounded" src="'.$item['photo'].'" alt="'.$item['groupname'].'" title="'.$item['groupname'].'" />
-	</a>
-	</div>
-	<div class="info">
-	<a href="'.tsUrl('group','show',array('id'=>$item[groupid])).'">'.$item['groupname'].'</a> ('.$count_user.')             
-	<div class="content">'.cututf8(t($item['groupdesc']),0,43,false).'</div>
-	</div>
-	</div>';
-	}
-
-	echo '</div>';
-	echo '<div class="clear"></div>';
-	echo '</div>';
-	
+    include template('recommendgroup','recommendgroup');
 }
-
 function recommendgroup_css(){
-
-	echo '<link href="'.SITE_URL.'plugins/home/recommendgroup/images/style.css" rel="stylesheet" type="text/css" />';
-
+	echo '<link href="'.SITE_URL.'plugins/home/recommendgroup/style.css" rel="stylesheet" type="text/css" />';
 }
-
 addAction('home_index_left','recommendgroup');
 addAction('pub_header_top','recommendgroup_css');
--- plugins/home/signuser/signuser.php
+++ plugins/home/signuser/signuser.php
@@ -7,7 +7,7 @@ function signuser(){
 	echo '<div class="panel-heading">最新签到用户</div>';
 	echo '<div class="panel-body"><div class="facelist"><ul>';
 	foreach($arrUser as $key=>$item){
-		echo '<li><a href="'.tsUrl('user','space',array('id'=>$item['userid'])).'"><img class="img-circle" src="'.$item['face'].'" alt="'.$item['username'].'" width="48" height="48" /></a><div class="username"><a href="'.tsUrl('user','space',array('id'=>$item['userid'])).'">'.$item['username'].'</a></div></li>';
+		echo '<li><a href="'.tsUrl('user','space',array('id'=>$item['userid'])).'"><img class="img-rounded" src="'.$item['face'].'" alt="'.$item['username'].'" width="48" height="48" /></a><div class="username"><a href="'.tsUrl('user','space',array('id'=>$item['userid'])).'">'.$item['username'].'</a></div></li>';
 	}
 	echo '</ul></div></div></div>';
 }
