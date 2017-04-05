dad99237ed11917029132997f2e5ea9adc611080
ThinkSAAS <thinksaas@qq.com>
2017-04-05 11:06:49 +0800

    小修复

---
+++ data/index.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+echo 'Thank you for using ThinkSAAS';
+echo '<br />';
+echo '<a style="color:#999" href="http://www.thinksaas.cn">www.thinksaas.cn</a>';
+exit;
