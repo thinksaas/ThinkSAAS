63bc3d92057416ea0332d267528c5399a4e62218
ThinkSAAS <thinksaas@qq.com>
2017-11-03 15:52:49 +0800

    抽出qq,weibo,豆瓣登录放到授权版里去了

--- plugins/pubs/ueditor/load.js
+++ plugins/pubs/ueditor/load.js
@@ -1,6 +1,6 @@
 //实例化编辑器
 var options = {
-	toolbars:[['FullScreen','Undo', 'Redo','removeformat','formatmatch','cleardoc','searchreplace','Bold','insertunorderedlist','insertorderedlist','forecolor','backcolor','fontsize','fontfamily','link','unlink','simpleupload','insertimage','music','attachment','insertvideo','emotion','insertcode','blockquote','inserttable','deletetable','mergeright','mergedown','splittorows','splittocols','splittocells','mergecells','insertcol','insertrow','deletecol','deleterow','insertparagraphbeforetable','map','template','wordimage','horizontal','anchor','spechars','autotypeset','pagebreak','drafts']],
+	toolbars:[['FullScreen','source','Undo', 'Redo','removeformat','formatmatch','cleardoc','searchreplace','Bold','insertunorderedlist','insertorderedlist','forecolor','backcolor','fontsize','fontfamily','link','unlink','simpleupload','insertimage','music','attachment','insertvideo','emotion','insertcode','blockquote','inserttable','deletetable','mergeright','mergedown','splittorows','splittocols','splittocells','mergecells','insertcol','insertrow','deletecol','deleterow','insertparagraphbeforetable','map','template','wordimage','horizontal','anchor','spechars','autotypeset','pagebreak','drafts']],
 	initialFrameWidth:'100%',
 	initialFrameHeight:420,
 	focus:false,
