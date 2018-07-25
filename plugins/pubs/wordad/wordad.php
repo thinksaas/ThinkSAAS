582c53ce1dde255100bfd3a67160b375a8ad78a8
ThinkSAAS <thinksaas@qq.com>
2018-07-25 22:13:42 +0800

    1、优化安装程序对新版本的检测提示，保证安装最新程序版本；2、优化部分细节

--- plugins/pubs/wordad/wordad.php
+++ plugins/pubs/wordad/wordad.php
@@ -8,7 +8,7 @@ function wordad(){
 		$arrData = $tsMySqlCache->get('plugins_pubs_wordad');
 	}
 	
-	echo '<div class="card rounded-0"><div class="card-body pb-1 pt-1"><div class="row">';
+	echo '<div class="card"><div class="card-body pb-1 pt-1"><div class="row">';
 	foreach($arrData as $key=>$item){
 		echo '<div class="col-md-3"><a class="m-2 mt-2 mb-2 float-left" target="_blank" href="'.$item['url'].'">'.$item['title'].'</a></div>';
 	}
--- upgrade/version.php
+++ upgrade/version.php
@@ -1,3 +1,3 @@
 <?php
 defined('IN_TS') or die('Access Denied.');
-return 2.6;
+return 2.61;
