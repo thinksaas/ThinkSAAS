1e88703061db481dc46dce8664ee07cba53fcf82
ThinkSAAS <thinksaas@qq.com>
2019-08-25 23:26:56 +0800

    [2.91]优化

--- app/group/action/admin/topic.php
+++ app/group/action/admin/topic.php
@@ -4,14 +4,47 @@ defined('IN_TS') or die('Access Denied.');
 switch($ts){
 	
 	case "list":
+
+        $isrecommend = intval($_GET['isrecommend']);
+        $istop = intval($_GET['istop']);
+
+        $topicid = intval($_GET['topicid']);
+
+        $title = urldecode($_GET['title']);
 		
 		$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 		$url = SITE_URL.'index.php?app=group&ac=admin&mg=topic&ts=list&page=';
 		$lstart = $page*10-10;
+
+
+        $where = null;
+
+        if($isrecommend==1){
+            $where = array(
+                'isrecommend'=>1,
+            );
+        }
+
+        if($istop==1){
+            $where = array(
+                'istop'=>1,
+            );
+        }
+
+
+        if($topicid){
+            $where = array(
+                'topicid'=>$topicid,
+            );
+        }
+
+        if($title){
+            $where = "`title` like '%$title%'";
+        }
 		
-		$arrTopic = $new['group']->findAll('group_topic',null,'addtime desc',null,$lstart.',10');
+		$arrTopic = $new['group']->findAll('group_topic',$where,'addtime desc',null,$lstart.',10');
 		
-		$topicNum = $new['group']->findCount('group_topic');
+		$topicNum = $new['group']->findCount('group_topic',$where);
 		
 		$pageUrl = pagination($topicNum, 10, $page, $url);
 
@@ -56,6 +89,67 @@ switch($ts){
 		qiMsg('操作成功！');
 	
 		break;
+
+
+    //推荐
+    case "isrecommend":
+
+        $topicid = intval($_GET['topicid']);
+
+        $strTopic = $new['group']->find('group_topic',array(
+            'topicid'=>$topicid,
+        ));
+
+        if($strTopic['isrecommend']==0){
+            $new['group']->update('group_topic',array(
+                'topicid'=>$topicid,
+            ),array(
+                'isrecommend'=>1,
+            ));
+        }
+
+        if($strTopic['isrecommend']==1){
+            $new['group']->update('group_topic',array(
+                'topicid'=>$topicid,
+            ),array(
+                'isrecommend'=>0,
+            ));
+        }
+
+        qiMsg('操作成功！');
+
+        break;
+
+    //置顶
+    case "istop":
+
+        $topicid = intval($_GET['topicid']);
+
+        $strTopic = $new['group']->find('group_topic',array(
+            'topicid'=>$topicid,
+        ));
+
+        if($strTopic['istop']==0){
+            $new['group']->update('group_topic',array(
+                'topicid'=>$topicid,
+            ),array(
+                'istop'=>1,
+            ));
+        }
+
+        if($strTopic['istop']==1){
+            $new['group']->update('group_topic',array(
+                'topicid'=>$topicid,
+            ),array(
+                'istop'=>0,
+            ));
+        }
+
+        qiMsg('操作成功！');
+
+        break;
+
+
 		
 	//删除的帖子
 	case "deletetopic":
